64669c8af3f6c91b6f2e8326e54e75b4db40826e
Juan Pablo Tamayo <jptamayo@gmail.com>
2022-01-05 00:37:25 -0300

    App operativa usando Valor UF dinamico

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
 "use strict"
 
+class Cotizador {
+    constructor(nombre, valor, uf) {
+        this.nombrePlan = nombre;
+        this.valorPlan = parseFloat(valor.replace(',', '.'));
+        this.valorUf = parseFloat(uf)
+    }
+    verValorCLP() {
+        var resultado = this.valorPlan * this.valorUf;
+        return resultado;
+    }
+}
 
-
-function obtener() {
+function obtenerUF() {
     var url = "https://api.cmfchile.cl/api-sbifv3/recursos_api/uf/?apikey=ee6a726a519cdb8f0c1ba1a6f1b42394314eab2e&formato=json"
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var data = xhr.responseText;
             var json = JSON.parse(data);
-            console.log("Peticion Finalizada", data);
-            console.log(json.UFs[0].Valor);
-            div.innerHTML = json.UFs[0].Valor;
-            // return json.UFs[0].Valor
+            var valorUF = json.UFs[0].Valor;
+            valorUF = valorUF.replace('.', '');
+            valorUF = valorUF.replace(',', '.');
+            valorUF = parseFloat(valorUF);
 
+            var cotizacion = new Cotizador(document.getElementById("plan").value, document.getElementById("valorUf").value, valorUF)
+            document.getElementById("resultado").innerHTML = cotizacion.verValorCLP()
         }
     }
-
     xhr.open("get", url)
     xhr.send();
-}
-
-const VALORUF = 30000;
-
-class Cotizador {
-    constructor(nombre, valor) {
-        this.nombrePlan = nombre;
-        this.valorPlan = valor;
-    }
-    verValorCLP() {
-        var resultado = this.valorPlan * VALORUF;
-        return resultado;
-    }
-}
-
-function crearCotizacion() {
-    var cotizacion = new Cotizador(document.getElementById("plan").value, document.getElementById("valorUf").value)
-    document.getElementById("resultado").innerHTML = cotizacion.verValorCLP()
-
 }
